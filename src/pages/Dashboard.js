4da927f69a3e4743e9cdd29108846eeb5d223eaa
Elena A. Pearson <epearson@lululemon.com>
2022-05-23 13:51:48 -0700

    cleanup

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,45 +1,23 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import NavLinks from "../layout/NavLinks"
 import CustomCard from "../components/CustomCard"
 import Grid from "@material-ui/core/Grid"
-import axios from "axios";
 import {ShowMoreHook} from './ShowMoreHook'
-
-// import "./styles.css";
-import Card from "@material-ui/core/Card";
+import { Button } from "@material-ui/core"
 
 export default function Dashboard() {
-    // const initialNumber = 5
-    // const [dataArray, setDataArray] = useState([]);
-    // const [visible, setVisible] = useState(5);
-    
-    // const allData = async () => {
-    //     const { data } = NavLinks;
-    //     setDataArray(data);
-    //   };
-    
-    //   const loadMore = () => {
-    //     setVisible(visible + 5);
-    //   };
-    
-    //   useEffect(() => {
-    //     allData();
-    //   }, []);
- 
+    const initialNumber = 5
     const { data } = NavLinks;
     const {loadMore, dataArray, visible} = ShowMoreHook({allItems: data})
-    
+
     return (
-        <Grid container spacing={3} >
-            {/* {data.map(item => (
-                <CustomCard key={item.name} card={item} />
-            ))} */}     
+        <Grid container spacing={3} >  
               {dataArray.slice(0, visible).map((item => (
                 <CustomCard key={item.name} card={item} />
             )))}
        
           {visible < dataArray.length && (
-             <button onClick={loadMore}>Load 5 More</button>
+              <Button color="primary" variant="outlined" style={{width: '200px', height: '50px', margin: '70px'}} onClick={loadMore}>Load {initialNumber} More</Button>
            )}
         </Grid>
     )
--- src/pages/ShowMoreHook.js
+++ src/pages/ShowMoreHook.js
@@ -1,12 +1,10 @@
 import React, {useEffect, useState} from 'react'
-// import NavLinks from "../layout/NavLinks"
 
 export const ShowMoreHook = ({allItems}) => {
     const [dataArray, setDataArray] = useState([]);
     const [visible, setVisible] = useState(5);
     
     const allData = async () => {
-        // const { data } = NavLinks;
         setDataArray(allItems);
       };
     
