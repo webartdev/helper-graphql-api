973a9295e627c1147339f45856d1a9fd9d602df9
Elena A. Pearson <epearson@lululemon.com>
2022-06-11 14:50:15 -0700

    resolve aws dependency conflict

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet'
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+// import L from 'leaflet'
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
@@ -41,34 +41,35 @@ export default function MapComponent() {
   const lng = 16;
   const zoom = 2.4;
   const position = [lat, lng];
-  const maxBounds = L.latLngBounds([
-    [-90, 180],
-    [90, -180]
-  ])
+  // const maxBounds = L.latLngBounds([
+  //   [-90, 180],
+  //   [90, -180]
+  // ])
 
 
 
   return (
-    <MapContainer
-      center={[20, 0]}
-      zoom={zoom}
-      scrollWheelZoom={true}
-      minZoom={2.4}
-      className={classes.mapContainer}
-      worldCopyJump={true}
-      maxBounds={maxBounds}
+    <div>Maps</div>
+    // <MapContainer
+    //   center={[20, 0]}
+    //   zoom={zoom}
+    //   scrollWheelZoom={true}
+    //   minZoom={2.4}
+    //   className={classes.mapContainer}
+    //   worldCopyJump={true}
+    //   maxBounds={maxBounds}
 
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          Marker showing a random location.
-      </Popup>
-      </Marker>
-    </MapContainer>
+    // >
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //   />
+    //   <Marker position={position}>
+    //     <Popup>
+    //       Marker showing a random location.
+    //   </Popup>
+    //   </Marker>
+    // </MapContainer>
   );
 }
 
